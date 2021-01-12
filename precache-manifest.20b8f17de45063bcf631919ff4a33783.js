966b3651737a2cd1d0bd39c83ee314f917821aba
Andriy Alieksieienko <andriy.alieksieienko@gmail.com>
2021-01-12 14:05:46 +0200

    Updates

---
+++ precache-manifest.20b8f17de45063bcf631919ff4a33783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b2faf5cf9b4476bc091fb60545b712",
+    "url": "./index.html"
+  },
+  {
+    "revision": "989411eca29ef72bacc8",
+    "url": "./static/css/main.ee6c1113.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "989411eca29ef72bacc8",
+    "url": "./static/js/main.d5b200e1.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
